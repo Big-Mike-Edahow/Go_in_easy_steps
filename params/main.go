df1a02805e1d35052528bed2d89d04b3acd5a91b
Mike Jackson <perigran_falcon@yahoo.com>
2024-08-01 16:27:42 -0600

    Minor tweaks.

--- params/main.go
+++ params/main.go
@@ -11,6 +11,5 @@ func main() {
 	for i, v := range os.Args {
 		fmt.Printf("Argument %v: %v \n", i, v)
 	}
-
-	fmt.Println("\nLast Argument:", os.Args[len(os.Args)-1])
+	fmt.Println("Last Argument:", os.Args[len(os.Args)-1])
 }
